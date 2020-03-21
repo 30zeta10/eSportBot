211e66363c33071c6e9cd5af5014f950e7037878
Toni Wirth <wirth.toni@fu-berlin.de>
2020-03-21 11:17:40 +0100

    clean up test file

--- test.py
+++ test.py
@@ -1,19 +1,3 @@
 from riotAPI import *
 
 print(game_info("geldin"))
-"""
-def test():
-    r = requests.get('http://ddragon.leagueoflegends.com/cdn/10.4.1/data/en_US/champion.json')
-
-
-    for i in list(r.json()['data']):
-        if r.json()['data'][i]['key'] == '245':
-            return(i)
-
-def printt():
-
-    print(test())
-
-
-printt()
-"""
