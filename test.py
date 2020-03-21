03d5a4f66e04a5cf6e080e6b618835b7888b8259
Toni Wirth <wirth.toni@fu-berlin.de>
2020-03-21 14:09:35 +0100

    started to develop a new bot_client

--- test.py
+++ test.py
@@ -1,3 +1,29 @@
+"""
 from riotAPI import *
 
 print(game_info("geldin"))
+"""
+# TOKEN = os.getenv('DISCORD_TOKEN')
+# ---   Libs    ---
+import discord
+import os
+
+# --- Constants ---
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+
+# Bot client class, to manage the bot
+class BotClient(discord.Client):
+    # Login message
+    async def on_ready(self):
+        print("eSportBot has connected.")
+
+    async def on_message(self, message):
+        if message.author == client:
+            return
+        print("Message from " + str(message.author) + " enthält " + str(message.content))
+
+
+# Start eSportBot
+client = BotClient()
+client.run(TOKEN)
